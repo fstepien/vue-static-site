ab24b84200b3202bcfa1e052c8366cef2d03efc1
fstepien <filip.stepien@gmail.com>
2019-04-07 08:19:33 -0400

    add page query to BlogPost.vue

--- src/templates/BlogPost.vue
+++ src/templates/BlogPost.vue
@@ -1,6 +1,21 @@
 <template lang="html">
-  <h1>New Template</h1>
+  <Layout>
+    <h1>{{ $page.blogPost.title }}</h1>
+    <p>{{ $page.blogPost.date }}</p>
+    <div v-html="$page.blogPost.content"></div>
+  </Layout>
 </template>
+
+<page-query>
+  query Post ($path: String!){
+  blogPost (path: $path){
+    title
+    content
+    tags
+    date (format: "MMM Do YYYY")
+  }
+}
+</page-query>
 
 <script>
 export default {};
