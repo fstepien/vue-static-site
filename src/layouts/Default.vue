f418650281a2b6cbe1cca3a0418640754abcc28d
fstepien <filip.stepien@gmail.com>
2019-04-20 10:07:11 -0400

    add sass loader

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -34,13 +34,14 @@ query {
 }
 </static-query>
 
-<style>
+<style lang="scss">
 body {
   font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
     "Helvetica Neue", Arial, sans-serif;
   margin: 0;
   padding: 0;
   line-height: 1.5;
+  font-size: $baseFontSize;
 }
 
 .layout {
@@ -48,14 +49,14 @@ body {
   margin: 0 auto;
   padding-left: 20px;
   padding-right: 20px;
-}
 
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+    height: 80px;
+  }
 }
 
 .nav__link {
